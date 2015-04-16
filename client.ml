6b81dc100348a620171e10a1b242c49bab142904
Chandan <chandan.yeshwanth@gmail.com>
2015-04-16 18:54:51 +0530

    file transfer working between client-server

--- client.ml
+++ client.ml
@@ -1,18 +1,68 @@
 open Unix
 open Printf
 
+let print_menu () = 
+	printf "
+			(1) Upload a file
+			(2) List files
+			(3) Get a file
+			(4) Remove a file
+		"
+	;;
+
+let read_file fname =
+  let channel = open_in fname in
+  let len = in_channel_length channel in
+  let str = String.create len in
+  really_input channel str 0 len;
+  close_in channel;
+  
+  (str)
+;;
+
+let upload_file sock = 
+	printf "Enter the name of the file you want to upload\n";
+
+	let fname = read_line () in 
+	let content = read_file fname in
+
+	send sock content 0 (String.length content) []
+;;	
+
+let list_files sock = 1
+;;
+
+let get_file sock = 1
+;;
+
+let remove_file sock = 1
+;;
+
 let run_client () = 
-	let client_sock = socket PF_INET SOCK_STREAM 0 in
-	connect client_sock (ADDR_INET(inet_addr_of_string "127.0.0.1", 12345));
+	while true do
+		let client_sock = socket PF_INET SOCK_STREAM 0 in
+		connect client_sock (ADDR_INET(inet_addr_of_string "127.0.0.1", 12345));
+		printf "Connected to server\n";
 
-	printf "Connected to server\n";
+		print_menu ();	
 
-	let buffer = String.create 512 in
-	let count = (recv client_sock buffer 0 512 []) in
+		let input = read_line () in
+		let action_func = 
+			match (int_of_string input) with
+			  1 -> upload_file
+			| 2 -> list_files
+			| 3 -> get_file
+			| 4 -> remove_file
+			| _ -> 
+				printf "Try again";
+				exit 0;
+			in
 
-	printf "%s\n" buffer;
+		action_func client_sock;
 
-	close client_sock;
+		printf "Closing connection\n";
+		close client_sock;	
+	done;
 ;;
 
 let () = run_client()
--- server.ml
+++ server.ml
@@ -1,21 +1,39 @@
 open Unix
 open Printf
 
+(* read everything pending in the socket *) 
+let readall socket = 
+    let buffer = String.create 512 in
+    let rec _readall accum = 
+      try 
+        let count = (recv socket buffer 0 512 []) in
+          if count = 0 then accum else _readall ((String.sub buffer 0 count)::accum)
+        with _ -> 
+          accum
+    in
+      String.concat "" (List.rev (_readall []))
+  ;;
+
 let run_server () = 
+
   let sock = socket PF_INET SOCK_STREAM 0 in
   bind sock (ADDR_INET(inet_addr_of_string "127.0.0.1", 12345));
   listen sock 5;
-  printf "Waiting for connection ...\n%!";
 
-  let (s, _) = accept sock in 
-  printf "Accepted a connection\n";
+  while true do
+    printf "Waiting for connection ...\n%!";
 
-  let cout =  out_channel_of_descr s in
-  fprintf cout "Abc%!";
+    let (s, _) = accept sock in 
+    printf "Accepted a connection\n";
 
-  printf "Closing connection\n";
+    let str = readall s in 
+    printf "%s" str;
 
-  close sock;
+    printf "Closing connection\n";
+  done;
+
+  close sock;  
+  
 ;;
 
 let () = run_server()
